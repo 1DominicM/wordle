e544b6d851699a677beaec8a7fffc48fada677d1
Dominic <125dmiguelcruz@frhsd.com>
2024-01-05 10:42:03 -0500

    squid

--- js/word.js
+++ js/word.js
@@ -21,8 +21,67 @@ function intialize() {
             let tile = document.createElement("span"); 
             tile.id=r.toString() + "-" + c.toString(); 
             tile.classList.add("tile")
-            tile.innerText = "P";
-            document.getElementById("board").appendChild(tile)
+            tile.innerText = "";
+            document.getElementById("board").appendChild(tile);
         }
     }
+    // Listen for keyup
+    document.addEventListener("keyup" , (e) =>{
+        if (gameOver) return;
+
+        // alert(e.code)
+        if("KeyA" <= e.code && e.code <= "KeyZ"){
+            if(col < width){
+                let currentTile = document.getElementById(row.toString() + "-" + col.toString());
+                if (currentTile.innerText == "") {
+                    currentTile.innerText = e.code[3];
+                    col ++;
+                }
+            }
+        }
+                else if (e.code == "Backspace") {
+                    if(0 < col && col <= width){
+                        col --;
+                    }
+                    let currentTile = document.getElementById(row.toString() + "-" + col.toString());
+                    currentTile.innerText = "";
+                
+            
+        }
+        else if (e.code == "Enter") {
+    update();
+    row += 1;
+    col = 0;
+        }
+
+        if (!gameOver && row == height) {
+            gameOver = true;
+            document.getElementById("answer").innerText = wrd;
+        }
+
+    }) 
+}
+
+function update() {
+    let correct = 0;
+    for (let c = 0; c < width; c++) {
+        let tile = document.getElementById(row.toString() + "-" + c);
+        let letter = tile.innerText;
+
+        if (word[c] == letter) {
+            tile.classList.add("correct");
+            correct++;
+        }
+        else if (word.includes(letter)) {
+            tile.classList.add("present");
+        }
+        else {
+            tile.classList.add("absent");
+        }
+        if (correct == width) {
+            gameOver = true;
+
+        }
+    }
+
 }
